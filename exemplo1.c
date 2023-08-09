ea966edb11b65055926bebf209012f845eba1f22
Rafael Gildin <59973774+rafaelgildin@users.noreply.github.com>
2023-08-09 20:41:12 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+  printf("Mackenzie - FCI\n");
+  printf("Algoritmos e ");
+  printf("Prog. II\n");
+  printf("Linguagem: C\n");
+  // pause antes de terminar o programa
+  return 0;
+}
