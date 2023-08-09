14139af0b619e0e57dd0dbaffc663e1883db7199
Rafael Gildin <59973774+rafaelgildin@users.noreply.github.com>
2023-08-09 23:47:46 +0000

    updates

--- exemplo1.c
+++ exemplo1.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main() {
-
   printf("Mackenzie - FCI\n");
   printf("Algoritmos e ");
   printf("Prog. II\n");
-  printf("Linguagem: C\n");
+  printf("Fundacao: 1970\n");
   // pause antes de terminar o programa
   return 0;
 }
