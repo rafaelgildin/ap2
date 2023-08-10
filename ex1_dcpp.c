85473dec740bb27b21621b092a635238cfb04424
rafael.gildin <rafagildin@gmail.com>
2023-08-09 22:09:35 -0300

    updates

---
+++ ex1_dcpp.c
@@ -0,0 +1,18 @@
+/*
+Links: https://github.com/rafaelgildin/ap2_2023s2_lab1
+replit: https://replit.com/@RafaelGildin/Atividade1#main.c
+*/
+
+#include <stdio.h>
+
+int main(){
+	
+	printf("Mackenzie - FCI\n");
+	printf("Algoritmos e ");
+	printf("Prog. II\n");
+	printf("Turma: 02N11\n");
+	
+	// pause antes de terminar o programa
+	system("pause");
+	return 0;
+}
